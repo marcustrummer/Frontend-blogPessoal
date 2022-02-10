178c0d94d9af3e37d0a3963e2d530eb71993ae60
marcustrummer <marcusstrummer1@gmail.com>
2022-02-10 18:04:33 -0300

    Add metodo atualizar na service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -32,6 +32,12 @@ export class AuthService {
     return this.http.post<User>('https://receitasdomarquito.herokuapp.com/usuarios/cadastrar', user)
   }
 
+  atualizar(user: User): Observable<User> {
+    return this.http.put<User>('https://receitasdomarquito.herokuapp.com/usuarios/atualizar', user)
+  }
+
+
+
   getByIdUser(id: number): Observable<User> {
     return this.http.get<User>(`https://receitasdomarquito.herokuapp.com/usuarios/${id}`, this.token)
   }
